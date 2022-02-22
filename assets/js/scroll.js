c778c23e19db3cda3bd7423c24702bbd2042426c
yellowuncle13 <cima103303043@gmail.com>
2022-02-22 23:12:27 +0800

    update scroll

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,17 +1,3 @@
-// Initialize Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyBItDXZqrN92UgcokiCBdFLL9pfY1Lxlqw",
-  authDomain: "tracyandken-8610a.firebaseapp.com",
-  projectId: "tracyandken-8610a",
-  storageBucket: "tracyandken-8610a.appspot.com",
-  messagingSenderId: "223376251660",
-  appId: "1:223376251660:web:65a731eb8e9ab9a8ac0729",
-  measurementId: "G-Z39MFM2JLG",
-  databaseURL: "https://tracyandken-8610a-default-rtdb.asia-southeast1.firebasedatabase.app/"
-};
-firebase.initializeApp(firebaseConfig);
-var db = firebase.database();
-
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -24,9 +10,8 @@ function topFunction() {
 // Redeem
 function redeem() {
   var value = prompt("想要許什麼願~?");
-  if (value != "") alert(`想${value}嘛? 罐罐遵命!`);
+  if (value != "" && value != null) alert(`想${value}嘛? 罐罐遵命!`);
 
-  
   // db.ref(`/accounts/Queen`).on('value', function (snapshot) {
   //   if (snapshot.exists()){
   //       var value = prompt("想要許什麼願~?");
