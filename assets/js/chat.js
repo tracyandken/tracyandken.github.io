78c021973180df1cbf9665e2a62d33caebeab3bc
yellowuncle13 <cima103303043@gmail.com>
2022-04-14 23:18:39 +0800

    update date

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -26,7 +26,6 @@ function getContent()
             <div class="chat-message-right pb-4">
                 <div>
                     <img src="https://bootdey.com/img/Content/avatar/avatar8.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
-                    <div class="text-muted small text-nowrap mt-2">2:33 am</div>
                 </div>
                 <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
                     <div class="font-weight-bold mb-1">Queen</div>
@@ -45,8 +44,7 @@ function response(content)
                 <div class="chat-message-left pb-4">
                     <div>
                         <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
-                        <div class="text-muted small text-nowrap mt-2">${_DateTimezone(8)}</div>
-                    </div>
+                        </div>
                     <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
                         <div class="font-weight-bold mb-1">罐罐</div>
                         ${content}
