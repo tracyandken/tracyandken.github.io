53c84916e643eaf5eab225b73ebf640af70d4025
yellowuncle13 <cima103303043@gmail.com>
2022-04-15 23:33:36 +0800

    update img

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -25,7 +25,7 @@ function getContent()
     chats += `
             <div class="chat-message-right pb-4">
                 <div>
-                    <img src="https://bootdey.com/img/Content/avatar/avatar8.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
+                    <img src="assets/img/tracy.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
                 </div>
                 <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
                     <div class="font-weight-bold mb-1">Queen</div>
@@ -35,23 +35,50 @@ function getContent()
             `
     $('#applyForm')[0].reset();
     _getData()
-    setTimeout(function (){response(random())}, 2000);
+    if(content.includes("test"))
+    {
+        setTimeout(function (){response(content)}, 2000);
+    }
+    else
+    {
+        setTimeout(function (){response(random())}, 2000);
+    }
 }
 
 function response(content)
 {
-    chats += `
-                <div class="chat-message-left pb-4">
-                    <div>
-                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
+    if(content.includes("test"))
+    {
+        call_jieba_cut(content, function (result) {
+            chats += `
+                    <div class="chat-message-left pb-4">
+                        <div>
+                            <img src="assets/img/ken.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
+                            </div>
+                        <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
+                            <div class="font-weight-bold mb-1">罐罐</div>
+                            ${result}
                         </div>
-                    <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
-                        <div class="font-weight-bold mb-1">罐罐</div>
-                        ${content}
                     </div>
+                `
+        _getData()
+        });
+    }
+    else
+    {
+        chats += `
+        <div class="chat-message-left pb-4">
+            <div>
+                <img src="assets/img/ken.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
                 </div>
-            `
-    _getData()
+            <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
+                <div class="font-weight-bold mb-1">罐罐</div>
+                ${content}
+            </div>
+        </div>
+    `
+        _getData()
+    }
 }
 
 function random()
@@ -68,23 +95,4 @@ function random()
 function getRandomInt(max)
 {
     return Math.floor(Math.random() * max);
-}
-
-// time
-function _DateTimezone(offset)
-{
-    d = new Date();
-    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-    var date = new Date(utc + (3600000 * offset)).toLocaleTimeString();
-    if (date.includes("上午"))
-    {
-        date = date.substring(2, 7);
-        date += ` am`;
-    }
-    else
-    {
-        date = date.substring(2, 7);
-        date += ` pm`;
-    }
-    return date;
 }
